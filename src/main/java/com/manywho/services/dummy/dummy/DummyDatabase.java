67ed7d146bf11fdb7269128f8ef8ebc7b59df0c7
xiaoqian.shi <xiaoqian.shi@dell.com>
2020-09-24 15:57:09 +0100

    feature/FLOW-2011 change the structure of the class to make skip/pagination work.

--- src/main/java/com/manywho/services/dummy/dummy/DummyDatabase.java
+++ src/main/java/com/manywho/services/dummy/dummy/DummyDatabase.java
@@ -16,8 +16,7 @@ public class DummyDatabase implements Database<ApplicationConfiguration, Dummy> 
 
     private static HashMap<String, Dummy> DUMMIES;
 
-    static
-    {
+    static {
         DUMMIES = DummyDatabaseDataBuilder.BuildDummies();
     }
 
@@ -28,42 +27,66 @@ public class DummyDatabase implements Database<ApplicationConfiguration, Dummy> 
 
     @Override
     public List<Dummy> findAll(ApplicationConfiguration configuration, ObjectDataType objectDataType, Command command, ListFilter filter, List<MObject> objects) {
-
-        Stream<Dummy> pagedDummies = DUMMIES
+        return DUMMIES
                 .values()
                 .stream()
-                .skip(filter.getOffset());
-
-        if (filter.getWhere().size()!=0 ){
-            String filterColumn = filter.getWhere().get(0).getColumnName();
-            if ( filter.getLimit() > 0 ) {
-                pagedDummies = pagedDummies
-                        .filter(c ->
-                        {
-                            if (filterColumn.equals("Id")) {
-                                return c.getId().equals(filter.getWhere().get(0).getContentValue());
-                            } else if (filterColumn.equals("Name")) {
-                                return c.getName().equals(filter.getWhere().get(0).getContentValue());
-                            } else if (filterColumn.equals("Age")) {
-                                return String.valueOf(c.getAge()).equals(filter.getWhere().get(0).getContentValue());
-                            } else if (filterColumn.equals("Bio")) {
-                                return c.getBio().equals(filter.getWhere().get(0).getContentValue());
-                            } else if (filterColumn.equals("Remote")) {
-                                return String.valueOf(c.getRemote()).equals(filter.getWhere().get(0).getContentValue());
-                            } else {
-                                System.out.println("Invalid filter columnName " + filterColumn);
-                                return false;
+                .filter(c ->
+                {
+                    if (filter == null || filter.getWhere() == null || filter.getWhere().size() == 0) {
+                        return true;
+                    }
+                    String filterColumn = filter.getWhere().get(0).getColumnName();
+                    return filterByColumnName(filter, c, filterColumn);
+                })
+                .sorted((a, b) -> {
+                            if (filter == null || filter.getOrderByDirectionType() == null) {
+                                return 1;
                             }
-                        })
-                        .limit(filter.getLimit());
-            }
-        }
-        else {
-            pagedDummies = pagedDummies
-                    .limit(filter.getLimit());
-        }
+                            String sortColumn = filter.getOrderByPropertyDeveloperName();
+                            return (filter.getOrderByDirectionType().toString() == "DESC") ?
+                                    propertyComparation(sortColumn, a, b) :
+                                    propertyComparation(sortColumn, b, a);
+                        }
+                )
+                .skip(filter.getOffset())
+                .limit(filter.getLimit())
+                .collect(Collectors.toList());
+    }
 
-        return pagedDummies.collect(Collectors.toList());
+    private boolean filterByColumnName(ListFilter filter, Dummy c, String filterColumn) {
+        if (filterColumn.equals("Id")) {
+            return c.getId().equals(filter.getWhere().get(0).getContentValue());
+        } else if (filterColumn.equals("Name")) {
+            return c.getName().equals(filter.getWhere().get(0).getContentValue());
+        } else if (filterColumn.equals("Age")) {
+            return String.valueOf(c.getAge()).equals(filter.getWhere().get(0).getContentValue());
+        } else if (filterColumn.equals("Bio")) {
+            return c.getBio().equals(filter.getWhere().get(0).getContentValue());
+        } else if (filterColumn.equals("Remote")) {
+            return String.valueOf(c.getRemote()).equals(filter.getWhere().get(0).getContentValue());
+        } else {
+            System.out.println("Invalid filter columnName " + filterColumn);
+            return false;
+        }
+    }
+
+    private int propertyComparation(String propertyName, Dummy a, Dummy b) {
+        if (propertyName.equals("Id")) {
+            return b.getId().compareTo(a.getId());
+        } else if (propertyName.equals("Name")) {
+            return b.getName().compareTo(a.getName());
+        } else if (propertyName.equals("Age")) {
+            return b.getAge() - a.getAge();
+        } else if (propertyName.equals("Bio")) {
+            return b.getBio().compareTo(a.getBio());
+        } else if (propertyName.equals("Remote")) {
+            return Boolean.compare(b.getRemote(), a.getRemote());
+        } else if (propertyName.equals("Hired")) {
+            return b.getHired().compareTo(a.getHired());
+        } else {
+            System.out.println("Invalid orderType name " + propertyName);
+            return 0;
+        }
     }
 
     @Override
@@ -75,33 +98,39 @@ public class DummyDatabase implements Database<ApplicationConfiguration, Dummy> 
     }
 
     @Override
-    public List<Dummy> create(ApplicationConfiguration applicationConfiguration, ObjectDataType objectDataType, List<Dummy> list) {
+    public List<Dummy> create(ApplicationConfiguration applicationConfiguration, ObjectDataType
+            objectDataType, List<Dummy> list) {
         list.forEach(dummy -> {
             dummy.setId(UUID.randomUUID().toString());
-            DUMMIES.put(dummy.getId(), dummy);});
+            DUMMIES.put(dummy.getId(), dummy);
+        });
 
         return new ArrayList<Dummy>(DUMMIES.values());
     }
 
     @Override
-    public void delete(ApplicationConfiguration applicationConfiguration, ObjectDataType objectDataType, Dummy dummy) {
+    public void delete(ApplicationConfiguration applicationConfiguration, ObjectDataType objectDataType, Dummy
+            dummy) {
 
     }
 
     @Override
-    public void delete(ApplicationConfiguration applicationConfiguration, ObjectDataType objectDataType, List<Dummy> list) {
+    public void delete(ApplicationConfiguration applicationConfiguration, ObjectDataType
+            objectDataType, List<Dummy> list) {
 
     }
 
     @Override
-    public Dummy update(ApplicationConfiguration applicationConfiguration, ObjectDataType objectDataType, Dummy dummy) {
+    public Dummy update(ApplicationConfiguration applicationConfiguration, ObjectDataType objectDataType, Dummy
+            dummy) {
         DUMMIES.put(dummy.getId(), dummy);
 
         return dummy;
     }
 
     @Override
-    public List<Dummy> update(ApplicationConfiguration applicationConfiguration, ObjectDataType objectDataType, List<Dummy> list) {
+    public List<Dummy> update(ApplicationConfiguration applicationConfiguration, ObjectDataType
+            objectDataType, List<Dummy> list) {
         return null;
     }
 }
