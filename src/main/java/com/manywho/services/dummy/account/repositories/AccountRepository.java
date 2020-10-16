9fd8c0f240b5e9698d3cf70b684ceef3011efdf2
xiaoqian.shi <xiaoqian.shi@dell.com>
2020-10-16 11:27:54 +0100

    feature/FLOW-579 extracted sql creation to a separate method and removed unused findAll method

--- src/main/java/com/manywho/services/dummy/account/repositories/AccountRepository.java
+++ src/main/java/com/manywho/services/dummy/account/repositories/AccountRepository.java
@@ -27,73 +27,84 @@ public class AccountRepository {
         }
     }
 
-    public Account findAll(String filterBy, String value) {
-        String sql = "SELECT * FROM account WHERE " + filterBy + "= :" + value;
-        try (Connection connection = sql2o.open()) {
-            return connection.createQuery(sql)
-                    .addParameter(filterBy, filterBy)
-                    .executeAndFetchFirst(Account.class);
-        }
-    }
-
     public List<Account> findAll(ListFilter filter) throws ServiceProblemException {
 
-        String sql = "";
+        String sql = "SELECT * FROM account ";
         if (filter != null && filter.getWhere() != null && filter.getWhere().size() != 0) {
-            String columnname = filter.getWhere().get(0).getColumnName();
-            String value = filter.getWhere().get(0).getContentValue();
+
+            if (filter.getWhere().size() > 1) {
+                throw new ServiceProblemException(400, "Currently we only support filtering on one value, multiple values have been are provided.");
+            }
+
+            String columnName = filter.getWhere().get(0).getColumnName();
+            String filterByValue = filter.getWhere().get(0).getContentValue();
             CriteriaType criteriaType = filter.getWhere().get(0).getCriteriaType();
 
-            if (columnname.equals("id")) {
-
-                if (criteriaType == CriteriaType.GreaterThan) {
-                    sql = "SELECT * FROM account WHERE id > " + value;
-                } else if (criteriaType == CriteriaType.GreaterThanOrEqual) {
-                    sql = "SELECT * FROM account WHERE id >= " + value;
-                } else if (criteriaType == CriteriaType.LessThan) {
-                    sql = "SELECT * FROM account WHERE id < " + value;
-                } else if (criteriaType == CriteriaType.LessThanOrEqual) {
-                    sql = "SELECT * FROM account WHERE id <= " + value;
-                } else if (criteriaType == CriteriaType.Equal) {
-                    sql = "SELECT * FROM account WHERE id == " + value;
-                } else if (criteriaType == CriteriaType.NotEqual) {
-                    sql = "SELECT * FROM account WHERE id != " + value;
-                } else if (criteriaType == CriteriaType.IsEmpty) {
-                    sql = (Boolean.valueOf(filter.getWhere().get(0).getContentValue())) ?
-                            "SELECT * FROM account WHERE id is null" :
-                            "SELECT * FROM account WHERE id is not null";
-                } else {
-                    throw new ServiceProblemException(400, "Integer type \"id\" only supports EQUAL, NOT_EQUAL, GREATER_THAN, GREATER_THAN_OR_EQUAL, LESS_THAN, LESS_THAN_OR_EQUAL and IS_EMPTY operations");
-                }
-            } else if (columnname.equals("company") || columnname.equals("name") || columnname.equals("stateid")) {
-
-                if (criteriaType == CriteriaType.StartsWith) {
-                    sql = "SELECT * FROM account WHERE " + columnname + " LIKE \'" + value + "%\'";
-                } else if (criteriaType == CriteriaType.EndsWith) {
-                    sql = "SELECT * FROM account WHERE " + columnname + " LIKE \'%" + value + "\'";
-                } else if (criteriaType == CriteriaType.IsEmpty) {
-                    sql = (Boolean.valueOf(filter.getWhere().get(0).getContentValue())) ?
-                            "SELECT * FROM account WHERE " + columnname + " is null" :
-                            "SELECT * FROM account WHERE " + columnname + " is not null";
-                } else if (criteriaType == CriteriaType.Contains) {
-                    sql = "SELECT * FROM account WHERE " + columnname + " LIKE \'%" + value + "%\'";
-                } else if (criteriaType == CriteriaType.Equal) {
-                    sql = "SELECT * FROM account WHERE " + columnname + " == \'" + value + "\'";
-                } else if (criteriaType == CriteriaType.NotEqual) {
-                    sql = "SELECT * FROM account WHERE " + columnname + " != \'" + value + "\'";
-                } else {
-                    throw new ServiceProblemException(400, "String CriteriaType only supports EQUAL, NOT_EQUAL, STARTS_WITH, ENDS_WITH, CONTAINS and IS_EMPTY operations.");
-                }
-            }
-        } else {
-            sql = "SELECT * FROM account ";
+            sql = createSQL(filter, sql, columnName, filterByValue, criteriaType);
         }
-
         try (
                 Connection connection = sql2o.open()) {
             return connection.createQuery(sql)
                     .executeAndFetch(Account.class);
         }
+    }
+
+    private String createSQL(ListFilter filter, String sql, String columnName, String value, CriteriaType criteriaType) {
+        if (columnName.equals("id")) {
+            switch (criteriaType) {
+                case GreaterThan:
+                    sql = sql + " WHERE " + columnName + " > " + value;
+                    break;
+                case GreaterThanOrEqual:
+                    sql = sql + " WHERE " + columnName + " >= " + value;
+                    break;
+                case LessThan:
+                    sql = sql + " WHERE " + columnName + " < " + value;
+                    break;
+                case LessThanOrEqual:
+                    sql = sql + " WHERE " + columnName + " <= " + value;
+                    break;
+                case Equal:
+                    sql = sql + " WHERE " + columnName + " = " + value;
+                    break;
+                case NotEqual:
+                    sql = sql + " WHERE " + columnName + " != " + value;
+                    break;
+                case IsEmpty:
+                    sql = (Boolean.valueOf(filter.getWhere().get(0).getContentValue())) ?
+                            sql + " WHERE " + columnName + " is null" :
+                            sql + " WHERE " + columnName + " is not null";
+                    break;
+                default:
+                    throw new ServiceProblemException(400, "Integer CriteriaType only supports EQUAL, NOT_EQUAL, GREATER_THAN, GREATER_THAN_OR_EQUAL, LESS_THAN, LESS_THAN_OR_EQUAL and IS_EMPTY operations");
+            }
+        } else if (columnName.equals("company") || columnName.equals("name") || columnName.equals("stateid")) {
+            switch (criteriaType) {
+                case StartsWith:
+                    sql = sql + " WHERE " + columnName + " LIKE \'" + value + "%\'";
+                    break;
+                case EndsWith:
+                    sql = sql + " WHERE " + columnName + " LIKE \'%" + value + "\'";
+                    break;
+                case IsEmpty:
+                    sql = (Boolean.valueOf(filter.getWhere().get(0).getContentValue())) ?
+                            sql + " WHERE " + columnName + " is null" :
+                            sql + " WHERE " + columnName + " is not null";
+                    break;
+                case Contains:
+                    sql = sql + " WHERE " + columnName + " LIKE \'%" + value + "%\'";
+                    break;
+                case Equal:
+                    sql = sql + " WHERE " + columnName + " = \'" + value + "\'";
+                    break;
+                case NotEqual:
+                    sql = sql + " WHERE " + columnName + " != \'" + value + "\'";
+                    break;
+                default:
+                    throw new ServiceProblemException(400, "String CriteriaType only supports EQUAL, NOT_EQUAL, STARTS_WITH, ENDS_WITH, CONTAINS and IS_EMPTY operations.");
+            }
+        }
+        return sql;
     }
 
     public Account update(Account account) {
