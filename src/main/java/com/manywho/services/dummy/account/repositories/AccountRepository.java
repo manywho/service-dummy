3d5149abc6618679e5e5115587e7e73211bbd25f
xiaoqian.shi <xiaoqian.shi@dell.com>
2020-10-19 17:05:26 +0100

    feature/FLOW-579 refactoring the code a bit by using StringBuilder and making createSQL method handling all cases.

--- src/main/java/com/manywho/services/dummy/account/repositories/AccountRepository.java
+++ src/main/java/com/manywho/services/dummy/account/repositories/AccountRepository.java
@@ -29,82 +29,111 @@ public class AccountRepository {
 
     public List<Account> findAll(ListFilter filter) throws ServiceProblemException {
 
-        String sql = "SELECT * FROM account ";
+        String sql = createSQL(filter);
+
+        try (Connection connection = sql2o.open()) {
+            return connection.createQuery(sql).executeAndFetch(Account.class);
+        }
+    }
+
+    private String createSQL(ListFilter filter) {
+        StringBuilder stringBuilder = new StringBuilder("SELECT * FROM account ");
+
         if (filter != null && filter.getWhere() != null && filter.getWhere().size() != 0) {
 
             if (filter.getWhere().size() > 1) {
                 throw new ServiceProblemException(400, "Currently we only support filtering on one value, multiple values have been are provided.");
             }
 
+            String value = filter.getWhere().get(0).getContentValue();
             String columnName = filter.getWhere().get(0).getColumnName();
-            String filterByValue = filter.getWhere().get(0).getContentValue();
             CriteriaType criteriaType = filter.getWhere().get(0).getCriteriaType();
 
-            sql = createSQL(filter, sql, columnName, filterByValue, criteriaType);
-        }
-        try (
-                Connection connection = sql2o.open()) {
-            return connection.createQuery(sql)
-                    .executeAndFetch(Account.class);
-        }
-    }
+            stringBuilder.append(" WHERE ");
+            stringBuilder.append(columnName);
 
-    private String createSQL(ListFilter filter, String sql, String columnName, String value, CriteriaType criteriaType) {
-        if (columnName.equals("id")) {
-            switch (criteriaType) {
-                case GreaterThan:
-                    sql = sql + " WHERE " + columnName + " > " + value;
-                    break;
-                case GreaterThanOrEqual:
-                    sql = sql + " WHERE " + columnName + " >= " + value;
-                    break;
-                case LessThan:
-                    sql = sql + " WHERE " + columnName + " < " + value;
-                    break;
-                case LessThanOrEqual:
-                    sql = sql + " WHERE " + columnName + " <= " + value;
-                    break;
-                case Equal:
-                    sql = sql + " WHERE " + columnName + " = " + value;
-                    break;
-                case NotEqual:
-                    sql = sql + " WHERE " + columnName + " != " + value;
-                    break;
-                case IsEmpty:
-                    sql = (Boolean.valueOf(filter.getWhere().get(0).getContentValue())) ?
-                            sql + " WHERE " + columnName + " is null" :
-                            sql + " WHERE " + columnName + " is not null";
-                    break;
-                default:
-                    throw new ServiceProblemException(400, "Integer CriteriaType only supports EQUAL, NOT_EQUAL, GREATER_THAN, GREATER_THAN_OR_EQUAL, LESS_THAN, LESS_THAN_OR_EQUAL and IS_EMPTY operations");
-            }
-        } else if (columnName.equals("company") || columnName.equals("name") || columnName.equals("stateid")) {
-            switch (criteriaType) {
-                case StartsWith:
-                    sql = sql + " WHERE " + columnName + " LIKE \'" + value + "%\'";
-                    break;
-                case EndsWith:
-                    sql = sql + " WHERE " + columnName + " LIKE \'%" + value + "\'";
-                    break;
-                case IsEmpty:
-                    sql = (Boolean.valueOf(filter.getWhere().get(0).getContentValue())) ?
-                            sql + " WHERE " + columnName + " is null" :
-                            sql + " WHERE " + columnName + " is not null";
-                    break;
-                case Contains:
-                    sql = sql + " WHERE " + columnName + " LIKE \'%" + value + "%\'";
-                    break;
-                case Equal:
-                    sql = sql + " WHERE " + columnName + " = \'" + value + "\'";
-                    break;
-                case NotEqual:
-                    sql = sql + " WHERE " + columnName + " != \'" + value + "\'";
-                    break;
-                default:
-                    throw new ServiceProblemException(400, "String CriteriaType only supports EQUAL, NOT_EQUAL, STARTS_WITH, ENDS_WITH, CONTAINS and IS_EMPTY operations.");
+            if (columnName.equals("id")) {
+                switch (criteriaType) {
+                    case GreaterThan:
+                        stringBuilder.append(" > ");
+                        stringBuilder.append(value);
+                        break;
+                    case GreaterThanOrEqual:
+                        stringBuilder.append(" >= ");
+                        stringBuilder.append(value);
+                        break;
+                    case LessThan:
+                        stringBuilder.append(" < ");
+                        stringBuilder.append(value);
+                        break;
+                    case LessThanOrEqual:
+                        stringBuilder.append(" <= ");
+                        stringBuilder.append(value);
+                        break;
+                    case Equal:
+                        stringBuilder.append(" = ");
+                        stringBuilder.append(value);
+                        break;
+                    case NotEqual:
+                        stringBuilder.append(" != ");
+                        stringBuilder.append(value);
+                        break;
+                    case IsEmpty:
+                        stringBuilder.append(" > ");
+                        stringBuilder.append(value);
+                        stringBuilder = (Boolean.valueOf(filter.getWhere().get(0).getContentValue())) ?
+                                stringBuilder.append(" is null ") :
+                                stringBuilder.append(" is not null");
+                        break;
+                    default:
+                        throw new ServiceProblemException(400, "Integer CriteriaType only supports EQUAL, NOT_EQUAL, GREATER_THAN, GREATER_THAN_OR_EQUAL, LESS_THAN, LESS_THAN_OR_EQUAL and IS_EMPTY operations");
+                }
+            } else if (columnName.equals("company") || columnName.equals("name") || columnName.equals("stateid")) {
+                switch (criteriaType) {
+                    case StartsWith:
+                        stringBuilder.append(" LIKE \'");
+                        stringBuilder.append(value);
+                        stringBuilder.append("%\'");
+                        break;
+
+                    case EndsWith:
+                        stringBuilder.append(" LIKE \'%");
+                        stringBuilder.append(value);
+                        stringBuilder.append("\'");
+                        break;
+
+                    case IsEmpty:
+                        stringBuilder = (Boolean.valueOf(filter.getWhere().get(0).getContentValue())) ?
+                                stringBuilder.append(" is null ") :
+                                stringBuilder.append(" is not null");
+                        break;
+
+                    case Contains:
+                        stringBuilder.append(" LIKE \'%");
+                        stringBuilder.append(value);
+                        stringBuilder.append("%\'");
+                        break;
+
+                    case Equal:
+                        stringBuilder.append(" = \'");
+                        stringBuilder.append(value);
+                        stringBuilder.append("\' ");
+
+                        break;
+
+                    case NotEqual:
+                        stringBuilder.append(" != \'");
+                        stringBuilder.append(value);
+                        stringBuilder.append("\'");
+
+                        break;
+
+                    default:
+                        throw new ServiceProblemException(400, "String CriteriaType only supports EQUAL, NOT_EQUAL, STARTS_WITH, ENDS_WITH, CONTAINS and IS_EMPTY operations.");
+                }
             }
         }
-        return sql;
+        return stringBuilder.toString();
     }
 
     public Account update(Account account) {
